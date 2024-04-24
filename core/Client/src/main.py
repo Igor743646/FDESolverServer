2b305cd2b2fbb56fd2505e8e6c8dc5c530540e89
Igor743646 <igor.743646@gmail.com>
2024-04-24 16:35:57 +0300

    update

---
+++ core/Client/src/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI(
+    title="FDESolver"
+)
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
